06db5f64c5e777cf654ea5cb2910146630bd3270
Pranjul Shukla <pranjs23@in.ibm.com>
2019-07-08 23:27:08 +0530

    Stylistic changes in UI

--- src/app/shared/components/auth-modal/auth-modal.component.ts
+++ src/app/shared/components/auth-modal/auth-modal.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'auth-modal-content',
+    styles: ['::ng-deep .mat-form-field-flex > .mat-form-field-infix { padding: 0.6em 0px !important;}',
+            '::ng-deep .mat-form-field-label-wrapper { top: -1.35em; }',
+            '::ng-deep .mat-form-field-appearance-outline.mat-form-field-can-float.mat-form-field-should-float .mat-form-field-label { transform: translateY(-1.2em) scale(.75);width: 133.33333%;}'],
     templateUrl: './auth-modal.component.html',
 })
 export class AuthModalContent {
