0177cecbedefd1cd1bfc3aa4df457c1e1252d3c9
Pranjul Shukla <pranjs23@in.ibm.com>
2019-07-18 11:11:21 +0530

    Refactored code season 2

--- src/app/shared/components/auth-modal/auth-modal.component.ts
+++ src/app/shared/components/auth-modal/auth-modal.component.ts
@@ -5,7 +5,9 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
     selector: 'auth-modal-content',
     styles: ['::ng-deep .mat-form-field-flex > .mat-form-field-infix { padding: 0.6em 0px !important;}',
             '::ng-deep .mat-form-field-label-wrapper { top: -1.35em; }',
-            '::ng-deep .mat-form-field-appearance-outline.mat-form-field-can-float.mat-form-field-should-float .mat-form-field-label { transform: translateY(-1.2em) scale(.75);width: 133.33333%;}'],
+            '::ng-deep .mat-form-field-appearance-outline.mat-form-field-can-float.mat-form-field-should-float .mat-form-field-label { transform: translateY(-1.2em) scale(.75);width: 133.33333%;}',
+            '.mw-80 { min-width:80%; }',
+            '.mw-25v { min-width: 25vw; }'],
     templateUrl: './auth-modal.component.html',
     providers: [{
         provide: STEPPER_GLOBAL_OPTIONS,
